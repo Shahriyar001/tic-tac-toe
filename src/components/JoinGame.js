c88485f10dfc46eb43b005c61c366cafdddecfa4
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-07-03 18:12:33 +0600

    add game channel

--- src/components/JoinGame.js
+++ src/components/JoinGame.js
@@ -1,18 +1,44 @@
 import React, { useState } from "react";
+import { useChatContext, userChatContext } from "stream-chat-react";
 
 const JoinGame = () => {
   const [rivalUsername, setRivalUsername] = useState("");
+  const { client } = useChatContext();
+  const [channel, setChannel] = useState(null);
+  const createChannel = async () => {
+    const response = await client.queryUsers({ name: { $eq: rivalUsername } });
+
+    if (response.users.length === 0) {
+      alert("Users not found");
+      return;
+    }
+
+    const newChannel = await client.channel("messaing", {
+      members: [client.userID, response.users[0].id],
+    });
+
+    await newChannel.watch();
+    setChannel(newChannel);
+  };
+
   return (
-    <div className="joinGame">
-      <h4>Crete Game</h4>
-      <input
-        placeholder="Username of rival"
-        type="text"
-        onChange={(event) => {
-          setRivalUsername(event.target.value);
-        }}
-      />
-    </div>
+    <>
+      {channel ? (
+        <h1>Game Started</h1>
+      ) : (
+        <div className="joinGame">
+          <h4>Create Game</h4>
+          <input
+            placeholder="Username of rival..."
+            type="text"
+            onChange={(event) => {
+              setRivalUsername(event.target.value);
+            }}
+          />
+          <button onClick={createChannel}>Join/Start Game</button>
+        </div>
+      )}
+    </>
   );
 };
 
